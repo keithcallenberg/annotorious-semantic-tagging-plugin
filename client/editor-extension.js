ae0345d2b00eb36f2a795814055ea096266411f5
Keith Callenberg <keithcallenberg@gmail.com>
2017-10-09 23:17:18 -0400

    revert

--- client/editor-extension.js
+++ client/editor-extension.js
@@ -46,7 +46,6 @@ annotorious.plugin.SemanticTagging.prototype._extendEditor = function(annotator)
         jqLink.addClass('accepted');
         delete topic.status;
         annotation.tags.push(topic);
-        annotator.editor._btnSave.click();
       }
     });
   };
