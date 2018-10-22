fa6912dc44094a344c633c75cb34a55c2ddce3df
Keith Callenberg <keithcallenberg@gmail.com>
2018-10-22 16:00:50 -0400

    fix setting of text

--- client/editor-extension.js
+++ client/editor-extension.js
@@ -26,7 +26,7 @@ annotorious.plugin.SemanticTagging.prototype._extendEditor = function(annotator)
       jqLink.addClass(opt_css_class);
 
     jqLink.click(function() {
-        $(".annotorious-editor-text").val(topic);
+        $(".annotorious-editor-text").val(topic.title);
      });
   };
 
